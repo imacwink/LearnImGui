fa2c0cda887b72aefa016b683ad47ca5804ab170
wangyungang <417024106@qq.com>
2021-02-05 11:39:54 +0800

    change latest imgui version

--- include/example/imgui_example.h
+++ include/example/imgui_example.h
@@ -1,6 +1,7 @@
 // System libs.
 #include <imgui.h>
-#include <imgui_impl_glfw_gl3.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 #include <stdio.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
